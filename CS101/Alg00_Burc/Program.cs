27c4df7299822ebb1215e3f088c063c16e67c319
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-01 20:28:53 +0300

    commit the CS101

---
+++ CS101/Alg00_Burc/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Dogum ayinizi giriniz: ");
+        int ay = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Dogum gunu giriniz: ");
+        int gun = Convert.ToInt32(Console.ReadLine());
+
+        if(ay==1 | ay == 01)
+        {
+            if(gun>=1 && gun <= 31)
+            {
+                if (gun <= 22)
+                {
+                    Console.WriteLine("Burcunuz Oglaktır.");
+                }
+                else
+                {
+                    Console.WriteLine("Burcunuz kovadir.");
+                }
+                       
+            }
+        }
+        else if(ay==2 | ay == 02)
+        {
+            if(gun>=1 && gun <= 29)
+            {
+                if (gun <= 19)
+                {
+                    Console.WriteLine("Burcunuz kovadır.");
+                }
+                else {
+                    Console.WriteLine("Burcunuz Baliktir."); 
+                }
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ CS101/ConsoleApp1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Security.Claims;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+        #region part00
+        string name;
+        string nameDesc;
+        string Name;
+        string NameDesc;
+
+        name = "Ertugrul Dagli "; // değişkene değer atama
+        nameDesc = "Benim adım : ";
+        NameDesc = "Hocamın adı : ";
+        Name = "Umit Karaçivi";
+
+        Console.WriteLine(nameDesc + name + "\n\n\n\t");
+        Console.WriteLine(NameDesc + Name);
+        #endregion
+
+        #region part01
+        byte a = 5;
+        byte a1 = 255;
+
+        int sum=a+a1;
+
+        Console.WriteLine("Toplam degeri = " + sum.ToString() + "\n\n");
+
+
+
+
+        #endregion
+
+        #region part02
+        byte s1 = 5, s2 = 15;
+        int topla=s1+s2;
+        string str1 = "merhaba ", str2 = "adim ertu";
+
+        Console.WriteLine("Sayilarin toplami: " + topla.ToString());
+        Console.WriteLine(str1 + str2);
+        #endregion
+
+        #region part03
+        //erkana isim basma
+        
+        Console.Write("Bir isim giriniz: ");
+        string isim=Convert.ToString(Console.ReadLine()); //isim=Console.ReadLine(); aynı görevi görür.
+        Console.WriteLine("Hos geldiniz " + isim + " bey");
+
+        #endregion
+
+        #region part04
+        //sık yapilan hatalar
+        // aynı satırda farklı veri tiplerinden tanımlama yapma.
+        //tanımlanan değişkene tanımlandığı tipten farklı değer atama.
+        //değişkenı tanımlamadan üzerine değer atama.
+        //değişkenin tanımlama yerinin yanlış yerde yapılması
+        //bazı değişken türlerinde (float) gerekli bazı tanımlamaların yapılmaması
+        // float veri tiplerinde virgül yerine nokta işareti kullanılır.
+
+        //string abc = "654654";
+        //string ab = "46545656";
+        //int cvb=Convert.ToInt32(abc)+ Convert.ToInt32(ab);
+            
+
+
+
+
+        #endregion
+
+        Console.ReadKey();
+
+
+
+    }
+}
